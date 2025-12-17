8fd8a0d46ccb9b400923c2886726d00afe741d5a
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2025-12-17 16:55:52 -0300

    Cuarto commit

--- app/src/main/java/com/example/pantallasnavegacion/pantallas/PantallaMenu.kt
+++ app/src/main/java/com/example/pantallasnavegacion/pantallas/PantallaMenu.kt
@@ -5,7 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,6 +29,7 @@ fun PantallaMenu(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        botonera()
         Text(text = "Elige una opción:", modifier = Modifier.padding(bottom = 32.dp))
 
         Button(onClick = onBoton1Click, modifier = Modifier.padding(8.dp)) {
@@ -36,6 +43,54 @@ fun PantallaMenu(
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun botonera(){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Small Top App Bar")
+                }
+            )
+        },
+    ) { innerPadding ->
+        ScrollContent(innerPadding)
+    }
+
+    /*
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.TopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Menu Principal")
+                }
+            )
+        }
+        bottomBar = {
+            Text(text = "Pantalla 1")
+        }
+    ){
+        innerPadding -> Modifier.padding(innerPadding)
+
+
+
+    }
+*/
+
+
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
